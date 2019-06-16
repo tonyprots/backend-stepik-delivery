2df08d2e5eed8b945b19fe8241d32a5008dae1d0
tony-prots <tony-prots@yandex-team.ru>
2019-06-17 02:20:20 +0300

    Middle of the Lesson 4

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
-promotion_text = 0
+promotion_text = "Скидка скидка 15%"
+
+stepik_alive = True
+
+workhours_opens = '10:00'
+workhours_closes = '22:00'
+
+promocode ="stepik"
 
 
 @app.route("/")
@@ -11,13 +18,23 @@ def hello():
 
 @app.route("/alive")
 def alive():
-    return'{"alive":true}'
+    if stepik_alive==True:
+        return'{"alive":true}'
 
 @app.route("/workhours")
 def workours():
-    return '{"opens":"10:00", "closes":"20:00"}'
+    return '{"opens": "'+workhours_opens+'", "closes":"'+workhours_closes+'"}'
 
-#@app.route("/promotion")
-#def
+@app.route("/promotion")
+def promotion():
+    return '{"promotion":"'+promotion_text+'"}'
+
+@app.route("/promo/<code>")
+def promo(code):
+    if code == promocode:
+        return '{"valid":True}'
+    else:
+        return '{"valid":False}'
+
 
 app.run("0.0.0.0",8000)
