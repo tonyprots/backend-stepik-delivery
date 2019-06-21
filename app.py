f908c7694ed02562d9a6199c875211f3d4423bfd
tonyprots <tony-prots@yandex.ru>
2019-06-22 00:56:02 +0200

    routes alive, workhours, promotions, promo added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import random
+import json
 app = Flask(__name__)
 
 promotion_text = "Скидка скидка 15%"
@@ -8,8 +10,35 @@ stepik_alive = True
 workhours_opens = '10:00'
 workhours_closes = '22:00'
 
-promocode ="stepik"
+promocodes =[
+    {"code":"doubletrouble","discount":10},
+    {"code": "delivery", "discount": 10},
+    {"code":"stepik", "discount":15},
+    {"code":"pleaseplease","discount":5},
+    {"code":"libertyordeath", "discount":100}
+    ]
 
+promotions = [
+    "Скидка 15% по промокоду STEPIK",
+    "Скидка 15% по промокоду DELIVERY",
+    "Скидка 5% на все напитки"
+]
+
+meals = [{
+"title":"Chicken",
+"id":1,
+"available":True,
+"picture":'',
+"price":20.0,
+"category":1
+}, {
+"title":"Milk",
+"id":2,
+"available":True,
+"picture":'',
+"price":10.0,
+"category":1
+}]
 
 @app.route("/")
 def hello():
@@ -31,10 +60,13 @@ def promotion():
 
 @app.route("/promo/<code>")
 def promo(code):
-    if code == promocode:
-        return '{"valid":True}'
-    else:
-        return '{"valid":False}'
+    for promocode in promocodes:
+        if promocode["code"] == code:
+            return json.dumps({"valid":True,"discount":promocode['discount']})
+    return json.dumps({"valid": False})
 
+@app.route("/meals")
+def meals_route():
+    return json.dumps(meals)
 
 app.run("0.0.0.0",8000)
