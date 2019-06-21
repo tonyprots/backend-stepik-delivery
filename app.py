c1b57eeebb6d0655d30416b58da1b128cb4341a1
tonyprots <tony-prots@yandex.ru>
2019-06-22 01:55:40 +0200

    alive configured

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import random
 import json
+
 app = Flask(__name__)
 
 promotion_text = "Скидка скидка 15%"
@@ -10,12 +11,12 @@ stepik_alive = True
 workhours_opens = '10:00'
 workhours_closes = '22:00'
 
-promocodes =[
-    {"code":"doubletrouble","discount":50},
+promocodes = [
+    {"code": "doubletrouble", "discount": 50},
     {"code": "illbeback", "discount": 25},
-    {"code":"stepik", "discount":25},
-    {"code":"pleaseplease","discount":5},
-    {"code":"libertyordeath", "discount":100}
+    {"code": "stepik", "discount": 25},
+    {"code": "pleaseplease", "discount": 5},
+    {"code": "libertyordeath", "discount": 100},
     {"code": "summer", "discount": 10}
 ]
 
@@ -26,48 +27,60 @@ promotions = [
 ]
 
 meals = [{
-"title":"Chicken",
-"id":1,
-"available":True,
-"picture":'',
-"price":20.0,
-"category":1
+    "title": "Chicken",
+    "id": 1,
+    "available": True,
+    "picture": '',
+    "price": 20.0,
+    "category": 1
 }, {
-"title":"Milk",
-"id":2,
-"available":True,
-"picture":'',
-"price":10.0,
-"category":1
+    "title": "Milk",
+    "id": 2,
+    "available": True,
+    "picture": '',
+    "price": 10.0,
+    "category": 1
 }]
+
 
 @app.route("/")
 def hello():
-    return "<h1>" +"Test" +"</h1"
+    return "<h1>" + "Test" + "</h1"
 
 
 @app.route("/alive")
 def alive():
-    if stepik_alive==True:
-        return'{"alive":true}'
+    config_file = open('config.json', 'r')
+    config_content = config_file.read()
+    data = json.loads(config_content)
+    config_file.close()
+    if stepik_alive == True:
+        return json.dumps(data)
+
 
 @app.route("/workhours")
 def workours():
-    return '{"opens": "'+workhours_opens+'", "closes":"'+workhours_closes+'"}'
+    return json.dumps({"opens": workhours_opens, "closes": workhours_closes})
+    # '{"opens": "'+workhours_opens+'", "closes":"'+workhours_closes+'"}'
+
 
 @app.route("/promo")
 def promotion():
-    return '{"promotion":"'+promotions[random.randint(0,2)]+'"}'
+    return json.dumps({"promotion": promotions[random.randint(0, 2)]})
+    # '{"promotion":"'+promotions[random.randint(0,2)]+'"}'
+
 
 @app.route("/promo/<code>")
 def promo(code):
     for promocode in promocodes:
         if promocode["code"] == code:
-            return json.dumps({"valid":True,"discount":promocode['discount']})
+            return json.dumps({"valid": True, "discount": promocode['discount']}, ensure_ascii=False)
     return json.dumps({"valid": False})
+
 
 @app.route("/meals")
 def meals_route():
     return json.dumps(meals)
 
-app.run("0.0.0.0",8000)
+
+app.run("0.0.0.0", 8000)
